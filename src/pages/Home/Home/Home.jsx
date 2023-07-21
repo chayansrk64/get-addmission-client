a0fdb0ec22e4c45049a7f59d57700deb2d5c7c9a
chayansrk64 <chayansrk64@gmail.com>
2023-07-22 02:08:15 +0600

    initial commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+ 
+const Home = () => {
+    return (
+        <div>
+            {/* components will be called here */}
+            <p>this is home section</p>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+ 
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div>
+            <p>this is footer</p>
+        </div>
+    );
+};
+
+export default Footer;
