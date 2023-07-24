d33deda748e0f517450175b49a688aa7d8462f39
chayansrk64 <chayansrk64@gmail.com>
2023-07-24 16:52:44 +0600

    search api tested

--- src/pages/Home/Colleges/Colleges.jsx
+++ src/pages/Home/Colleges/Colleges.jsx
@@ -1,4 +1,4 @@
-import {  useState } from 'react';
+import { useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle';
 import './Colleges.css'
 import { useEffect } from 'react';
@@ -8,7 +8,7 @@ const colleges = () => {
 
     const [colleges, setColleges] = useState([]);
 
-    // // for search 
+    // for search 
     // const searchRef = useRef(null);
     // const [search, setSearch] = useState('');
     
@@ -16,7 +16,7 @@ const colleges = () => {
 
     useEffect(()=> {
         // fetch(`http://localhost:5000/college?search=${search}`)
-         fetch('http://localhost:5000/college')
+        fetch('http://localhost:5000/college')
         .then(res => res.json())
         .then(data => setColleges(data))
     },[])
@@ -35,8 +35,8 @@ const colleges = () => {
             <div className=''>
             <div className="form-control">
                 <div className="input-group justify-center">
-                    <input   type="text"   placeholder="Search…" className="input input-bordered" />
-                    <button   className="btn btn-square">
+                    <input    type="text"   placeholder="Search…" className="input input-bordered" />
+                    <button    className="btn btn-square">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
                 </div>
